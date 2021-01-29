516e80be4b55a415fde611681004203b892e8b10
Michaek Luk <52308484+LuniumLuk@users.noreply.github.com>
2021-01-29 21:46:00 +0800

    Thrust

---
+++ Assets/Scripts/BasicAttr.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicAttr : MonoBehaviour
+{
+    static int sequence = 0;
+    public int id = 0;
+    public float mass = 10;
+    public Vector3 spd;
+    public Vector3 acc;
+    public float Ressistance = 0.001f;
+
+    private void Start()
+    {
+        id = sequence++;
+        transform.localScale = Vector3.one * Mathf.Sqrt(mass);
+    }
+    private void FixedUpdate()
+    {
+        spd += acc * Time.fixedDeltaTime;
+        transform.position += spd * Time.fixedDeltaTime;
+        CheckBoundary();
+    }
+
+    void CheckBoundary()
+    {
+        Vector3 pos = Camera.main.WorldToScreenPoint(transform.position);
+        Vector3 norm = Vector3.zero;
+        if (pos.x > Screen.width)
+        {
+            norm = Vector3.left;
+        }
+        if (pos.x < 0)
+        {
+            norm = Vector3.right;
+        }
+        if (pos.y < 0)
+        {
+            norm = Vector3.up;
+        }
+        if (pos.y > Screen.height)
+        {
+            norm = Vector3.down;
+        }
+        if (norm != Vector3.zero)
+        {
+            spd = Vector2.Reflect(spd, norm);
+            norm = Vector3.zero;
+        }
+    }
+
+}
---
+++ Assets/Scripts/Thrust.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Thrust : MonoBehaviour
+{
+    BasicAttr basicAttr = null;
+
+    public float thrustForce = 0.001f;
+
+    private void Awake() {
+        basicAttr = GetComponent<BasicAttr>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButton(0)) {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mousePos.z = 0;
+            basicAttr.spd += (transform.position - mousePos).normalized * thrustForce * Time.deltaTime;
+        }
+        
+    }
+}
