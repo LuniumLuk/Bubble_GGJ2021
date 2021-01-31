b7087d296f68578b586c08b801f690e2c71678ce
PPG <49444331+s71799285@users.noreply.github.com>
2021-01-31 09:28:00 +0800

    更新 BasicAttr.cs

--- Assets/Scripts/BasicAttr.cs
+++ Assets/Scripts/BasicAttr.cs
@@ -22,7 +22,7 @@ public class BasicAttr : MonoBehaviour
     private void Start()
     {
         // 自增序号
-        if (id > 0)
+        if (id >= 0)
             id = sequence++;
         // 将scale设置为质量的平方根
         targetmass = mass;
